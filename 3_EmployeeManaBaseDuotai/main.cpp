9a34e2744930f4d698461da5ee75d3c08b67d450
KunWang123 <1062622524@qq.com>
2022-06-05 22:37:40 +0800

    add 使用多态的职工管理系统

---
+++ 3_EmployeeManaBaseDuotai/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"workManager.h"
+using namespace std;
+
+int main(){
+    workManager wm;
+    wm.ShowMeau();
+    return 0;
+}
---
+++ 3_EmployeeManaBaseDuotai/workManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+class workManager
+{
+private:
+    /* data */
+public:
+    workManager(/* args */);
+    void showmeau();
+    ~workManager();
+};
